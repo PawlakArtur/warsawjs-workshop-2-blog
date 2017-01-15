0f5ee00f1bced75174a4ac1eed5a8831ef5e1898
Artur Pawlak <pawlak.artur90@gmail.com>
2017-01-15 15:49:35 +0100

    add new comment

--- app/scripts/components/post.component.js
+++ app/scripts/components/post.component.js
@@ -8,7 +8,6 @@ export class PostComponent extends Component {
     }
 
     init() {
-
     }
 
     destroy() {
--- app/scripts/services/post.service.js
+++ app/scripts/services/post.service.js
@@ -1,7 +1,8 @@
 
 let posts = [
     {id: 1, title: 'pierwszy post', text: 'tresc posta'},
-    {id: 2, title: 'drugi post', text: 'tresc posta drugiego'}
+    {id: 2, title: 'drugi post', text: 'tresc posta drugiego'},
+    {id: 3, title: 'trzeci post', text: 'tresc posta trzeciego'}
 ]
 export class PostService {
 
@@ -13,7 +14,7 @@ export class PostService {
 
     static create(data) {
         data.id = posts.length+1;
-        posts.unshift(data);
+        posts.push(data);
         return new Promise((resolve, reject) => {
             resolve(data);
         })
@@ -23,5 +24,9 @@ export class PostService {
         return new Promise((resolve, reject) => {
             resolve(posts[parseInt(id)-1]);
         })
+    }
+
+    static delete(id) {
+        console.log("delete " + id);
     }
 }
