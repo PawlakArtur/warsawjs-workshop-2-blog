ba3c7ac03e3b1538176d95246ca72da2b318bb9f
Artur Pawlak <pawlak.artur90@gmail.com>
2017-01-15 17:25:11 +0100

    change to json server

--- app/scripts/services/post.service.js
+++ app/scripts/services/post.service.js
@@ -1,34 +1,18 @@
-
-let posts = [
-    {id: 1, title: 'pierwszy post', text: 'tresc posta'},
-    {id: 2, title: 'drugi post', text: 'tresc posta drugiego'},
-    {id: 3, title: 'trzeci post', text: 'tresc posta trzeciego'}
-]
 export class PostService {
-
     static list() {
-        return new Promise((resolve, reject) => {
-            resolve(posts);
-        })
+        return $.ajax('/posts');
     }
 
     static create(data) {
-        data.id = posts.length+1;
-        posts.push(data);
-        return new Promise((resolve, reject) => {
-            resolve(data);
-        })
+        return $.post('/posts', data);
     }
 
     static get(id) {
-        return new Promise((resolve, reject) => {
-            for(let post of posts) {
-                if(post.id === parseInt(id)) {
-                    return resolve(post);
-                }
-            }
-            reject('not found');
-        })
+        return $.get('/posts/' + id)
+    }
+
+    static getComment(postId) {
+
     }
 
     static delete(id) {
