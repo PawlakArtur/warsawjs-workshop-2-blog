6c5728a52abbdc6603fc6dff654aa3a3d01a79f7
Artur Pawlak <pawlak.artur90@gmail.com>
2017-01-15 14:46:42 +0100

    add post detail component

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,4 +4,4 @@ console.log('main.js działa!')
 import indexController from 'controllers/index.controller.js';
 import aboutController from 'controllers/about.controller.js';
 import writeController from 'controllers/write.controller.js';
-//import postController from 'controllers/post.controller.js';
+import postController from 'controllers/post.controller.js';
